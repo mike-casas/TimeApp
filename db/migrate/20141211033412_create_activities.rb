304a2b80ff84eb1111d5f7f25f519993a0f269f7
mike-casas <neneriostb@gmail.com>
2014-12-10 23:44:37 -0500

    generate models-controller and views

---
+++ db/migrate/20141211033412_create_activities.rb
@@ -0,0 +1,12 @@
+class CreateActivities < ActiveRecord::Migration
+  def change
+    create_table :activities do |t|
+      t.text :descripcion
+      t.date :started_at
+      t.date :ended_at
+      t.references :Project, index: true
+
+      t.timestamps
+    end
+  end
+end
